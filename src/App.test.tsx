7e86a6341cffff975b2f3a751ef49fd6e16fee8f
elijah lovejoy <elovejoy@ondema.io>
2022-11-28 09:07:44 -0800

    first pass at test-utils to wrap test with react-query QueryClientProvider and react-router MemoryRluter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,31 +1,9 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "./utils/test-utils";
 import { AppRoutes } from "./App";
-import { MemoryRouter } from "react-router-dom";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve({ foo: 1 }),
-  })
-) as jest.Mock;
-
-const queryClient = new QueryClient();
 
 test("App can render without blowing up", () => {
-  // TODO: https://tanstack.com/query/v4/docs/guides/testing
-  // use <MemoryRouter> when you want to manually control the history
-  // https://testing-library.com/docs/example-react-router/
-  render(
-    <QueryClientProvider client={queryClient}>
-      <MemoryRouter initialEntries={["/sbweather/"]}>
-        <AppRoutes />
-      </MemoryRouter>
-    </QueryClientProvider>
-  );
+  render(<AppRoutes />);
   const linkElement = screen.getByText(/Loading.../i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test.todo("show data if query succeeds");
-test.todo("show dummy data if query fails");
