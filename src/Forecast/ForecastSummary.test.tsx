0e2ef48cd9962e14896ccd7ad8a0d2be503e73a8
Elijah Lovejoy <elijah@lovejoy.com>
2025-01-28 19:05:11 -0800

    get tests working again

--- src/Forecast/ForecastSummary.test.tsx
+++ src/Forecast/ForecastSummary.test.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "../utils/test-utils";
 import { ForecastSummary } from "./ForecastSummary";
 import { getSampleForecast } from "./util";
 
 it("renders shortForecast && detailed forecast when getting at least one forecast", () => {
-  render(<ForecastSummary forecast={getSampleForecast()} />);
+  const { getByText } = render(
+    <ForecastSummary forecast={getSampleForecast()} />
+  );
   expect(
-    screen.getByText(
+    getByText(
       "Patchy fog between 11pm and 5am. Mostly cloudy, with a low around 54. East northeast wind 5 to 10 mph."
     )
   ).toBeInTheDocument();
-  expect(screen.getByText("Patchy Fog")).toBeInTheDocument();
+  expect(getByText("Patchy Fog")).toBeInTheDocument();
 });
 
 /**
@@ -19,16 +21,16 @@ it("renders shortForecast && detailed forecast when getting at least one forecas
  */
 it("renders error when arg undefined", () => {
   // @ts-ignore: Type 'undefined' is not assignable to type 'NwsForecast'
-  render(<ForecastSummary forecast={undefined} />);
+  const { getByText } = render(<ForecastSummary forecast={undefined} />);
   expect(
-    screen.getByText("Sorry, forecast does not appear to have loaded")
+    getByText("Sorry, forecast does not appear to have loaded")
   ).toBeInTheDocument();
 });
 
 it("renders error when arg []", () => {
   // @ts-ignore: Type 'never[]' has no properties in common with type 'NwsForecast'
-  render(<ForecastSummary forecast={[]} />);
+  const { getByText } = render(<ForecastSummary forecast={[]} />);
   expect(
-    screen.getByText("Sorry, forecast does not appear to have loaded")
+    getByText("Sorry, forecast does not appear to have loaded")
   ).toBeInTheDocument();
 });
