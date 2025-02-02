684056be9b211a84c3d15f5dd8f4da051a48b04a
Elijah Lovejoy <elijah@lovejoy.com>
2025-02-02 11:42:27 -0800

    update npm packages

--- src/About/index.tsx
+++ src/About/index.tsx
@@ -7,41 +7,45 @@ export const About = () => (
     </p>
     <h2>Ingredients</h2>
     <table border={1}>
-      <tr>
-        <td>Ingredient</td>
-        <td>Notes</td>
-        <td>Link(s)</td>
-      </tr>
-      <tr>
-        <td>Cursor</td>
-        <td>Much more like pairing than I thought it would be</td>
-        <td>
-          <div>
-            <a href="https://www.cursor.com/">https://www.cursor.com/</a>
-          </div>
-          <div>
-            <a href="https://cursor.directory/">https://cursor.directory/</a>
-          </div>
-        </td>
-      </tr>
-      <tr>
-        <td>NWS API</td>
-        <td>Forecast data</td>
-        <td>
-          <a href="https://www.weather.gov/documentation/services-web-api">
-            APIs
-          </a>
-        </td>{" "}
-      </tr>
-      <tr>
-        <td>MUI</td>
-        <td>UI components</td>
-        <td>
-          <div>
-            <a href="https://mui.com/">https://mui.com/</a>
-          </div>
-        </td>
-      </tr>
+      <thead>
+        <tr>
+          <th>Ingredient</th>
+          <th>Notes</th>
+          <th>Link(s)</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Cursor</td>
+          <td>Much more like pairing than I thought it would be</td>
+          <td>
+            <div>
+              <a href="https://www.cursor.com/">https://www.cursor.com/</a>
+            </div>
+            <div>
+              <a href="https://cursor.directory/">https://cursor.directory/</a>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>NWS API</td>
+          <td>Forecast data</td>
+          <td>
+            <a href="https://www.weather.gov/documentation/services-web-api">
+              APIs
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td>MUI</td>
+          <td>UI components</td>
+          <td>
+            <div>
+              <a href="https://mui.com/">https://mui.com/</a>
+            </div>
+          </td>
+        </tr>
+      </tbody>
     </table>
 
     <h2>Notes</h2>
--- src/Forecast/index.tsx
+++ src/Forecast/index.tsx
@@ -5,21 +5,20 @@ import { getForecast } from "./util";
 import { ForecastWithDummyData } from "./ForecastWithDummyData";
 
 export const Forecast = () => {
-  const { isLoading, isError, data, error } = useQuery({
+  const { isPending, isError, data, error } = useQuery({
     queryKey: ["nwsForecast"],
     queryFn: getForecast,
     retry: false,
-    staleTime: 300000, // 5 min cache = 1000*60*5
+    gcTime: 300000, // 5 min cache = 1000*60*5
   });
-  if (isLoading) return <div>"Loading..."</div>;
+
+  if (isPending) return <div>"Loading..."</div>;
 
   if (isError || data === undefined) {
     console.log(`getForecast returned error: ${error}`);
     return <ForecastWithDummyData />;
   }
 
-  // default: render a summary of the forecast
-  console.log("getForecast returned data: ", data);
   return (
     <div>
       <ForecastSummary forecast={data} />
